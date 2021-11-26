afc51159e12da5ba1110276f22a915878159be27
HyungGeun-Cho <chris1503@snu.ac.kr>
2021-11-26 22:56:08 +0900

    feat #5 : frontend backend connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
+  const [tempPong, setTempPong] = useState(false);
+
   useEffect(() => {
-    axios
-        .get(`http://13.124.30.252:8080/ping`, {}
-        )
-        .then((response) => {
-            console.log("check");
-      console.log(response.data);
+    axios.get(`http://13.124.30.252:8080/ping/`, {}).then((response) => {
+      console.log("response check");
+      console.log(response);
+      setTempPong(true);
+    }).catch((error) => {
+        console.log(error);
     });
-  });
+  }, []);
 
   console.log("test");
 
   return (
-    <div>
-      <h1>Hello World!</h1>;
+    <div className={"App"}>
+      <h1>Hello World!</h1>
+      <h1>pong : {tempPong}</h1>
     </div>
   );
 }
