f76a5803d74d96dacef38fd25e8f43eee4026dd2
ongdyub <ongdyub>
2021-12-25 15:36:50 +0900

    temporary Logic

--- src/Component/MainPage/Header/Header.js
+++ src/Component/MainPage/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
         setIsOpen(true);
     }
     const handleSearch = () => {
-        history.push('/search');
+        history.replace('/search');
     }
     const handleLogo = () => {
         history.replace("");
@@ -30,7 +30,7 @@ const Header = () => {
                      src="https://wafflestudio.com/_next/image?url=%2Fimages%2Ficon_intro.svg&w=640&q=75"
                      alt="waffle_studio"/>
             </a>
-            <div className="main-title" onClick={handleLogo}>walog</div>
+            <div className="main-title" onClick={handleLogo}>Walog</div>
             <IoSearchOutline className="search-icon" onClick={handleSearch}/>
             <button className="btn-write" onClick={handleLogin}>새 글 작성</button>
             <button className="btn-login" onClick={handleLogin}>로그인</button>
