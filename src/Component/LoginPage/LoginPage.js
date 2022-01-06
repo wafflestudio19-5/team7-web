d16a99cabaf46d6a2a1eda4d6d6c4b65f5ac1453
ongdyub <ongdyub>
2022-01-06 19:12:06 +0900

    add catch

--- src/Component/LoginPage/LoginPage.js
+++ src/Component/LoginPage/LoginPage.js
@@ -24,6 +24,7 @@ const LoginPage = () => {
                 console.log(registerCode);
                 console.log(response);
             })
+            .catch((error) => {});
     }, []);
 
     return(
