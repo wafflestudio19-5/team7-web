9997cc06fec454f908ac13c0d94857a3efd3619b
HyungGeun-Cho <chris1503@snu.ac.kr>
2022-01-11 23:42:04 +0900

    loginmodal proxy

--- src/Component/LoginModal/LoginModal.js
+++ src/Component/LoginModal/LoginModal.js
@@ -68,7 +68,7 @@ const LoginModal = (props) => {
       setDisable(true);
 
       axios
-        .post(`https://waflog.kro.kr/api/v1/auth/user`, {
+        .post(`/api/v1/auth/user`, {
           email: emailInput,
         })
         .then((response) => {
