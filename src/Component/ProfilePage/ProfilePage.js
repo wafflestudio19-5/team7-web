050b518d8960abf78da9b68d216e0dd14dee8d25
ongdyub <ongdyub>
2022-01-17 00:27:19 +0900

    userPost list

--- src/Component/ProfilePage/ProfilePage.js
+++ src/Component/ProfilePage/ProfilePage.js
@@ -2,15 +2,67 @@ import './ProfilePage.scss';
 import { ToastContainer, toast } from "react-toastify";
 import { useParams, useHistory } from "react-router-dom";
 import Header from '../MainPage/Header/Header';
+import UserPost from "./UserPost/UserPost";
+import {useEffect, useRef, useState} from "react";
+import axios from "axios";
 
 const ProfilePage = () => {
 
     const params = useParams();
     const history = useHistory();
     const URLSearch = new URLSearchParams(window.location.search);
+    const postPageRef = useRef({});
+
+    const [word, setWord] = useState("");
+    const [userPost, setUserPost] = useState([]);
+    const [postPageNumber, setPostPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios
+            .get(`/api/v1/user/@${params.userId}/search`, {
+                params: {
+                    page: 0,
+                    size: 6
+                },
+            })
+            .then((response) => {
+                console.log(response.data);
+                setUserPost(response.data.content);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+    }, []);
+
+    const handleScroll = () => {
+        const scrollTop = postPageRef.current.scrollTop;
+        const scrollHeight = postPageRef.current.scrollHeight;
+        const clientHeight = postPageRef.current.clientHeight;
+
+        if (scrollHeight - scrollTop - clientHeight < 10) {
+            if (!(postPageNumber === null)) {
+                axios
+                    .get(`/api/v1/user/@${params.userId}/search`, {
+                        params: {
+                            page: postPageNumber,
+                            size: 6
+                        },
+                    })
+                    .then((response) => {
+                        setUserPost(userPost.concat(response.data.content));
+                        if (response.data.last === true) {
+                            setPostPageNumber(null);
+                        } else {
+                            setPostPageNumber(postPageNumber + 1);
+                        }
+                    });
+            }
+        }
+    };
 
     return(
-        <div className="profilepage">
+        <div className="profilepage" ref={postPageRef} onScroll={handleScroll}>
             <ToastContainer/>
             <Header pageTitle={`${params.userId}.log`}/>
             <div className="all-container">
@@ -30,8 +82,6 @@ const ProfilePage = () => {
                     <div className="user-link">
 
 
-
-
                     </div>
                 </div>
                 <div className="post-type">
@@ -40,7 +90,11 @@ const ProfilePage = () => {
                     </div>
                 </div>
                 <div className="userpost-list">
-
+                    <ul className="search-list">
+                        {userPost.map((item) => (
+                            <UserPost item={item} userId={params.userId} key={item.id} />
+                        ))}
+                    </ul>
                 </div>
             </div>
         </div>
---
+++ src/Component/ProfilePage/UserPost/UserPost.js
@@ -0,0 +1,36 @@
+import "./UserPost.scss";
+import {useHistory, useParams} from "react-router-dom";
+import dayjs from "dayjs";
+
+const UserPost = ({ item }) => {
+    const params = useParams();
+    const URLSearch = new URLSearchParams(window.location.search);
+    const history = useHistory();
+
+    const handlePostClick = () => {
+        history.push("/post/@" + params.userId + "/" + item.url);
+    };
+
+    return (
+        <div className="search-item">
+            {item.thumbnail === null || item.thumbnail === "" ? (
+                <div />
+            ) : (
+                <div className="thumbnail" onClick={handlePostClick}>
+                    <img className="thumbnail-img" src={item.thumbnail} alt="썸네일" />
+                </div>
+            )}
+            <div className="post-mainsection" onClick={handlePostClick}>
+                <div className="title">{item.title}</div>
+                <div className="summary">{item.summary}</div>
+                <div className="tags"></div>
+                <div className="sub-info">
+                    {dayjs(item.createAt).format("YYYY년 MM월 DD일")} · {item.comments}
+                    개의 댓글
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserPost;
