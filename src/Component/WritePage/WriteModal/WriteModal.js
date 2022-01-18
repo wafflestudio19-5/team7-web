d2d5cef50303ef633b23e16a3ba7120b91dd522d
ongdyub <ongdyub>
2022-01-18 19:00:57 +0900

    image

--- src/Component/WritePage/WriteModal/WriteModal.js
+++ src/Component/WritePage/WriteModal/WriteModal.js
@@ -40,7 +40,7 @@ const WriteModal = (props) => {
       formData.append('image',event.target.files[0]);
     }
 
-    axios.post(`/api/v1/image?url=${url}`,
+    axios.post(`/api/v1/image?url=`,
       formData
     ,{
       headers: {
