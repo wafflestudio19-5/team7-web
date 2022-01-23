fa98a5d28c08d751435973f24ba60e9e3675aa59
ongdyub <ongdyub>
2022-01-23 16:54:38 +0900

    post tag href

--- src/Component/ProfilePage/ProfilePage.js
+++ src/Component/ProfilePage/ProfilePage.js
@@ -6,6 +6,8 @@ import UserPost from "./UserPost/UserPost";
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { IoIosSearch } from "react-icons/io";
+import { AiTwotoneMail, AiOutlineGithub, AiOutlineTwitter, AiFillFacebook, AiFillHome } from "react-icons/ai";
+import {useSessionContext} from "../../Context/SessionContext";
 
 const ProfilePage = () => {
   const params = useParams();
@@ -14,6 +16,7 @@ const ProfilePage = () => {
   const URLSearch = new URLSearchParams(search);
   const postPageRef = useRef({});
   const initialParams = URLSearch.get("q") === null ? "" : URLSearch.get("q");
+  const { handleLogout, id, isLogin, userId, token } = useSessionContext();
 
   const [word, setWord] = useState(initialParams);
   const [userName, setUserName] = useState("");
@@ -22,22 +25,84 @@ const ProfilePage = () => {
   const [userPost, setUserPost] = useState([]);
   const [postPageNumber, setPostPageNumber] = useState(0);
 
+  const [eE,setEE] = useState(false);
+  const [eG,setEG] = useState(false);
+  const [eF,setEF] = useState(false);
+  const [eT,setET] = useState(false);
+  const [eH,setEH] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [userGit, setUserGit] = useState("");
+  const [userFace, setUserFace] = useState("");
+  const [userTwit, setUserTwit] = useState("");
+  const [userHome, setUserHome] = useState("");
+
+  const [tagMenuList, setTagMenuList] = useState([]);
+
+  const setUserLink = (email, home, g, f, t) => {
+    setUserEmail(email);
+    setUserHome(home);
+    setUserGit(g);
+    setUserFace(f);
+    setUserTwit(t);
+
+    if(email !== null && email !== "" && email.length !== 0){
+      setEE(true);
+    }
+    else{
+      setEE(false);
+    }
+    if(g !== null && g !== ""){
+      setEG(true);
+    }
+    else{
+      setEG(false);
+    }
+    if(f !== null && f !== ""){
+      setEF(true);
+    }
+    else{
+      setEF(false);
+    }
+    if(t !== null && t !== ""){
+      setET(true);
+    }
+    else{
+      setET(false);
+    }
+    if(home !== null && home !== ""){
+      setEH(true);
+    }
+    else{
+      setEH(false);
+    }
+  }
+
   useEffect(() => {
     axios
-      .get(`/api/v1/user/@${params.userId}`, {
-        params: {},
-      })
-      .then((response) => {
-        setUserName(response.data.name);
-        setUserImg(response.data.image);
-        setUserShort(response.data.shortIntro);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+        .get(`/api/v1/user/@${params.userId}`, {
+          headers: {
+            Authentication: token,
+          },
+          params: {},
+        })
+        .then((response) => {
+          setUserName(response.data.name);
+          setUserImg(response.data.image);
+          setUserShort(response.data.shortIntro);
+          setUserLink(response.data.publicEmail, response.data.homepage, response.data.githubId, response.data.facebookId, response.data.twitterId)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  },[]);
+
+  useEffect(() => {
 
     axios
       .get(`/api/v1/user/@${params.userId}/search`, {
+        headers: {
+          Authentication: token,
+        },
         params: {
           keyword: word,
           page: 0,
@@ -73,6 +138,9 @@ const ProfilePage = () => {
       if (!(postPageNumber === null)) {
         axios
           .get(`/api/v1/user/@${params.userId}/search`, {
+            headers: {
+              Authentication: token,
+            },
             params: {
               keyword: word,
               page: postPageNumber,
@@ -111,7 +179,43 @@ const ProfilePage = () => {
             </div>
           </div>
           <div className="horizon-line" />
-          <div className="user-link"></div>
+          <div className="user-link">
+            {eE ?
+                <a className="user-link-social" target="_blank" href={`mailto:${userEmail}`} rel="noopener noreferrer">
+                  <AiTwotoneMail className="user-social-icon"/>
+                </a>
+                :
+                null
+            }
+            {eG ?
+                <a className="user-link-social" target="_blank" href={`https://github.com/${userGit}`} rel="noopener noreferrer">
+                  <AiOutlineGithub className="user-social-icon"/>
+                </a>
+                :
+                null
+            }
+            {eT ?
+                <a className="user-link-social" target="_blank" href={`https://twitter.com/${userTwit}`} rel="noopener noreferrer">
+                  <AiOutlineTwitter className="user-social-icon"/>
+                </a>
+                :
+                null
+            }
+            {eF ?
+                <a className="user-link-social" target="_blank" href={`https://facebook.com/${userFace}`} rel="noopener noreferrer">
+                  <AiFillFacebook className="user-social-icon"/>
+                </a>
+                :
+                null
+            }
+            {eH ?
+                <a className="user-link-social" target="_blank" href={`${userHome}`} rel="noopener noreferrer">
+                  <AiFillHome className="user-social-icon"/>
+                </a>
+                :
+                null
+            }
+          </div>
         </div>
         <div className="post-type">
           <div className="select-type">
@@ -134,6 +238,14 @@ const ProfilePage = () => {
             <UserPost item={item} userId={params.userId} key={item.id} />
           ))}
         </ul>
+        <div className="tag-menubar">
+          <div className="tag-wrapper">
+            <div className="tag-title">태그 목록</div>
+            <ul className="tag-contents">
+              ㅇ
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
