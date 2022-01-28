c3e0570b8856a3d69c4088d6e1f79397f233ad25
HyungGeun-Cho <chris1503@snu.ac.kr>
2022-01-28 18:06:21 +0900

    SettingPage homepage placehoder modified (https://)

--- src/Component/SettingPage/SettingPage.js
+++ src/Component/SettingPage/SettingPage.js
@@ -441,7 +441,7 @@ const SettingPage = () => {
                                                     </div>
                                                     <div className="social-list">
                                                         <AiFillHome className="social-icon"/>
-                                                        <input className="social-input" placeholder="홈페이지 주소를 입력하세요." value={userHome} onChange={handleHome}/>
+                                                        <input className="social-input" placeholder="홈페이지 주소를 입력하세요. (https:// 등 포함!)" value={userHome} onChange={handleHome}/>
                                                     </div>
                                                     <div  className="btn-wrapper">
                                                         <button className="social-cancel" onClick={setInSocial(false)}>취소</button>
